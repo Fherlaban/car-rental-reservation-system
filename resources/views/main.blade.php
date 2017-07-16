c7c90567f9bc292235d29d246579f083c3d12c7c
morls <moravel752@gmail.com>
2017-07-15 22:23:40 -0700

    first bacht

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+@extends('template.default')
+
+@section('styles')
+
+@endsection
+
+
+@section('contents')
+	<h1>Hi Content here</h1>
+@endsection
+
+
+@section('scripts')
+
+@endsection
---
+++ resources/views/template/default.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Car Rental Reservation System</title>
+        @yield('styles')
+    </head>
+    <body>
+    	@yield('contents')
+        
+    </body>
+    	@yield('scripts')
+</html>
